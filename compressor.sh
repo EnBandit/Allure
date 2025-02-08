7fbf35c55719b7e5e0df9a17ebe1b9f2183d3556
EnBandit <80570702+EnBandit@users.noreply.github.com>
2025-02-09 01:33:43 +0800

    新的css
    适应triliumNext

--- compressor.sh
+++ compressor.sh
@@ -19,7 +19,7 @@ clip=clear
 theme=(clear night)
 
 cd `dirname "$0"`
-source_file="source-code.css"
+source_file="New.css"
 
 for (( i = 0; i <= ${#theme[@]}; i++ )); do
     min_file="Allure-"${theme[i]}".css"
